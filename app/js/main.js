a9af491bb3e4d9d727cf2efd33b00ebe76c096fc
Georgiy Bukharov <souljorje@gmail.com>
2017-07-02 15:53:13 +0500

    Half work done

---
+++ app/js/main.js
@@ -0,0 +1,15 @@
+$('.images-popup').magnificPopup({
+  type:'image',
+  gallery: {
+      enabled: true
+    },
+  mainClass: 'mfp-with-zoom',
+  zoom: {
+    enabled: true, 
+    duration: 300, 
+    easing: 'ease-in-out', 
+    opener: function(openerElement) {
+      return openerElement.is('img') ? openerElement : openerElement.find('img');
+    }
+  }
+});
